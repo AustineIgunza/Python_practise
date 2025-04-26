7d1dd4d1baa941b8bc5474f4fd1838dc506e1448
AustineIgunza <Austine.igunza@strathmore.edu>
2025-04-26 13:03:10 +0300

    function  to give us the factorial of a number by subsequently multiplying the numbers between 1 and the next number after the given number

---
+++ factorial_loop.py
@@ -0,0 +1,12 @@
+#function helps us to calculate the factorial of a number
+
+def factorial_loop(r):
+    #sets the default value as 1
+    result = 1
+    #multiplies the values between 1 and the next number after the given number
+    for i in range(1,r+1):
+        result *= i
+    return result
+
+#example case use of the factorial function using the number 20
+print(factorial_loop(20))
